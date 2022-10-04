47e197955ec48c58ec3fbfeee79790a6002036f9
GrayHead <ss.zhuravlov@gmail.com>
2022-10-04 19:56:52 +0300

    clear branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import Users from "./components/users/Users";
 
 function App() {
 
 
-
     return (<div>
-        <h2>app component header</h2>
-        <Users/>
 
     </div>);
 }
