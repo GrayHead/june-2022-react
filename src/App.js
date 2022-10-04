eb89fbc39a861cd5b1d78091b3af2b89aeea160b
GrayHead <ss.zhuravlov@gmail.com>
2022-10-04 20:49:59 +0300

    redux base dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {LOAD_COMMENTS, loadCommnets} from "./index";
 
 function App() {
 
+    let state = useSelector(state => state.userReducer);
+    console.log(state);
+    let dispatch = useDispatch();
+
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                dispatch(loadCommnets(value));
+            });
+
+    }, []);
 
     return (<div>
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,86 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+
+
+const userReducer = (state = {users: [], user: null}, action) => {
+    switch (action.type) {
+        case "LOAD_USERS":
+            return {
+                ...state,
+                users: action.payload
+            };
+        case "CHOOSE_USER":
+            let id = action.payload;
+            let user = state.users.find(value => value.id === id);
+            return {...state, user: user}
+
+        default:
+            return state;
+
+    }
+
+
+}
+
+
+const postReducer = (state = {posts: [], post: null}, action) => {
+    switch (action.type) {
+        case "LOAD_POSTS":
+            return {
+                ...state,
+                posts: action.payload
+            };
+        case "CHOOSE_POST":
+            let id = action.payload;
+            let post = state.posts.find(value => value.id === id);
+            return {...state, post}
+
+        default:
+            return state;
+
+    }
+}
+
+
+export const LOAD_COMMENTS = "LOAD_COMMNETS";
+export const loadCommnets = (payload)=> {
+    return {type :LOAD_COMMENTS,payload}
+
+}
+const commnetReducer = (state = {comments: [], comment: null}, action) => {
+    switch (action.type) {
+        case LOAD_COMMENTS:
+            return {
+                ...state,
+                comments: action.payload
+            };
+        case "CHOOSE_COMMNET":
+            let id = action.payload;
+            let comment = state.comments.find(value => value.id === id);
+            return {...state, comment}
+        default:
+            return state;
+
+    }
+}
+
+let reducer = combineReducers({
+    userReducer,
+    postReducer,
+    commnetReducer
+});
+
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
